ca4ca0e3ab74e3f3a7e7a646da85f5edceea1f36
Gabriele Arnulfo <gabriele.arnulfo@gmail.com>
2013-11-29 15:58:43 +0100

    Merge FCSVWriter

--- include/FCSVWriter.h
+++ include/FCSVWriter.h
@@ -20,33 +20,6 @@ class FCSVWriter: public AbstractWriter{
 
 int FCSVWriter::update() 
 {
-<<<<<<< Updated upstream
-  // open file for writing 
-  // handle the exception for permission denied 
-  // no space left on device 
-  
-  checkFilename_();
-  
-  ofstream of;
-  of.open(getFilename().c_str());
-  assert(of.is_open());
-  
-  ClinicalFrame::ConstElectrodeIterator const_elec_it;
-  try{
-    // for each electorde in clinical frame
-    for(const_elec_it = begin(); const_elec_it != end(); const_elec_it++){
-      // write down all the contacts that belongs to one electrode
-      of<<(*const_elec_it);
-    }
-    
-    of.close();
-  }
-  catch(ostream::failure e){
-    // add logging information
-    cerr<<" "<<endl;
-  }
-  
-=======
 	
 	checkFilename_();
 
@@ -71,7 +44,6 @@ int FCSVWriter::update()
 
 	return 1;
 
->>>>>>> Stashed changes
 }
 
 #endif //FCSV_WRITER
