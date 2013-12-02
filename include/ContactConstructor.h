ee6ca300f148e1559691730197fe3c1f5fa176c4
Massimo Narizzano <mnarizzano@gmail.com>
2013-12-02 23:40:36 +0100

    Test subject2 passed. Modified the update in ContactConstuct.h and the entry point calculator (iterate over the first point)

--- include/ContactConstructor.h
+++ include/ContactConstructor.h
@@ -1,6 +1,6 @@
 //TODO : 
 //  - Scrivere la descrizione dell'algoritmo di update per doxygen
-//  - Fattorizzare meglio l'algoritmo di update.
+//  - Le costanti usate, farle diventare tali
 #ifndef CONTACT_CONSTRUCTOR_H
 #define CONTACT_CONSTRUCTOR_H
 
@@ -28,9 +28,11 @@ class ContactConstructor {
   RegionType retriveRegionAroundContact_(VoxelPointType index, int regionDim);
   void printContact_(string name,int  number,PhysicalPointType point);
   double getValue_(PhysicalPointType point);
-  void computeNewEntry_(PhysicalPointType* contact, PhysicalPointType entry, PhysicalPointType target);
+  PhysicalPointType computeNewEntry_(PhysicalPointType entry, PhysicalPointType target);
   PhysicalPointType getContact_(PhysicalPointType c, PhysicalPointType c1, PhysicalPointType c2, double distance);
   PhysicalPointType getBestContact_(PhysicalPointType prevContact, PhysicalPointType nextContact);
+  void printRegion_(RegionType region);
+
 };
 
 /*!  This function takes a set of Electrodes where only target and
@@ -51,22 +53,25 @@ void ContactConstructor::update( void ){
     printContact_(electrodeItr->getName(),0,entryPoint);
     printContact_(electrodeItr->getName(),0,targetPoint);
     
-    PhysicalPointType contact1 = targetPoint;
+    PhysicalPointType contact1;
     PhysicalPointType contact2;
-    double distance = 3.1;
-    if (getValue_(entryPoint) == 0) 
-      computeNewEntry_(&entryPoint, entryPoint, contact1); // If the point is into the void ....
+    if (getValue_(entryPoint) == 0) entryPoint = computeNewEntry_(entryPoint, targetPoint); // If the point is into the void ....
     
-    contact1 = getBestContact_(entryPoint,entryPoint);
+    contact1 = entryPoint;
     electrodeItr->addContact(contact1);
-    contactNum++;
-    contact2 = getBestContact_(contact1,getContact_(contact1,contact1,targetPoint,distance));
+    contactNum++; 
+    double distance = 3.1;
+    do {
+      contact2 = getBestContact_(contact1,getContact_(contact1,entryPoint,targetPoint,distance));
+      distance += 0.5;
+    }while((getValue_(contact2) < 1000) || (contact2.EuclideanDistanceTo(contact1) < 0.1));
     electrodeItr->addContact(contact2);
     contactNum++;
     
+    distance = 3.1;
     do {
-      nextContact = getBestContact_(contact2,getContact_(contact2,contact1,contact2,distance));
-      if(contact2.EuclideanDistanceTo(nextContact) > 1.0) { //contact2!=nextContact (decimals)
+      nextContact = getBestContact_(contact1,getContact_(contact2,entryPoint,contact2,distance));
+      if(contact1.EuclideanDistanceTo(nextContact) > 1.0) { //contact1!=nextContact (decimals)
 	electrodeItr->addContact(nextContact);
 	contact1 = contact2;
 	contact2 = nextContact;
@@ -75,8 +80,7 @@ void ContactConstructor::update( void ){
       } else {
 	distance += 0.5;
       }
-    } while ((targetPoint.EuclideanDistanceTo(entryPoint) > (entryPoint.EuclideanDistanceTo(nextContact) + 3.5)) && (distance <= 4.5)); 
-    // TODO: 1.5 e' un numero a caso per evitare che salti l'ultimo dei contatti.
+    } while ((targetPoint.EuclideanDistanceTo(entryPoint) > (entryPoint.EuclideanDistanceTo(contact1) + 3.5)) && (distance <= 10.5)); 
     electrodeItr++;
   }
 }
@@ -97,20 +101,17 @@ RegionType ContactConstructor::retriveRegionAroundContact_(VoxelPointType index,
   SizeType size;
   RegionType region;
   SpacingType spacing = ctImage_->GetSpacing();
-   
   for(unsigned int i=0;i<3;i++) {
-    size[i]     = 2*((long int)(ceil(spacing[i] * regionDim)+1));
+    size[i]     = 2*((long int)(ceil(spacing[i] * regionDim)))+1;
     index[i]   -= (long int)(ceil(spacing[i] * regionDim));
   }
-  size[2]=(long int)ceil(size[1]-1); // ???? why ????
   region.SetSize(size);
   region.SetIndex(index);
+  /* printRegion_(region); */ // tmp
   return region;
 }
 
 void ContactConstructor::printContact_(string name,int number,PhysicalPointType point) {
-  //headFrame_->fromLPS2RAS_(&point);
-  //headFrame_->fromRefToCenter_(&point);
   cout << name << number<<","<< point[0] << "," << point[1] << "," << point[2] <<",1,1" <<endl;  
 }
 
@@ -120,77 +121,29 @@ double ContactConstructor::getValue_(PhysicalPointType point) {
   return ctImage_->GetPixel(voxelPoint);
 }
 
-// [TODO] refactoring
-// It tries to find an entry point that does not have null value :
-// 1) First in an incremental region around the entry point if looks
-//    for a voxel that has value != 0.
-// 2) From this point it looks for the point having maximal momentum (p)
-// 3) Then the new entry is the projection of the entry point on the
-//    line from p to target. (i.e. the intersection betwen line p-target
-//    and the line passing for the entry and ortogonal to the line
-//    p-target
-void ContactConstructor::computeNewEntry_(PhysicalPointType* contact, PhysicalPointType entry, PhysicalPointType target){
-  int incrementalDistance = 3;
-  PhysicalPointType  physicalEntry = entry;
-  VoxelPointType     voxelEntry;
-  PhysicalPointType  pippo;
+void ContactConstructor::printRegion_(RegionType region){
+  itk::ImageRegionIterator<ImageType> imageIterator(ctImage_,region);
+  VoxelPointType p;
+  PhysicalPointType tmp;  
 
-  typename ImageType::RegionType region;
-  //translatePhysicalPoint_(&physicalEntry);
-  ctImage_->TransformPhysicalPointToIndex(physicalEntry,voxelEntry);
-
-  do{
-    region = retriveRegionAroundContact_(voxelEntry,incrementalDistance);
-    itk::ImageRegionIterator<ImageType> imageIterator(ctImage_,region);
-    VoxelPointType p;
-    PhysicalPointType tmp;  
-    int num = 0;
-    while(!imageIterator.IsAtEnd()){
-      p = imageIterator.GetIndex();
-      // TODO 1000 e' una stima a caso, dovrebbe forse essere
-      // calcolata sui punti neri della CT? Si prende cioe' una
-      // immagine grossa come tutta la CT e poi si itera e si calcola la
-      // media/varianza/min/max e si trova la costante
-      if (ctImage_->GetPixel(p) > 1000) {
-	
-	FilterType::Pointer filter = FilterType::New();
-	CalculatorType::Pointer calculator = CalculatorType::New();
-	filter->SetInput(ctImage_);
-	filter->SetRegionOfInterest(retriveRegionAroundContact_(p,3));
-	calculator->SetImage(filter->GetOutput());
-	try {
-	  filter->Update();
-	  calculator->Compute();
-	  (*contact)[0] = calculator->GetCenterOfGravity()[0];
-	  (*contact)[1] = calculator->GetCenterOfGravity()[1];
-	  (*contact)[2] = calculator->GetCenterOfGravity()[2];
-	} catch (itk::ExceptionObject &ex) {
-	  cerr<<"Error computeNew Entry"<<__LINE__<<ex.what()<<endl;
-	  return;
-	}
-	ctImage_->TransformIndexToPhysicalPoint(p,*contact);
-	// TODO una volta trovato un contatto 
-
-	//undoTranslatePhysicalPoint_(contact);
-	return;
-      }
-      ++imageIterator;
-      num++;
-    }
-    
-    incrementalDistance += 2;
-  }while (incrementalDistance < 10);
+  while(!imageIterator.IsAtEnd()){
+    p = imageIterator.GetIndex();
+    ctImage_->TransformIndexToPhysicalPoint(p,tmp);
+    printContact_("",0,tmp);
+    //cout << "0,"<<p << endl;
+    ++imageIterator;
+  }
 }
-
 
 PhysicalPointType ContactConstructor::getBestContact_(PhysicalPointType prevContact, PhysicalPointType nextContact){
   VoxelPointType    nextVoxel;    
   FilterType::Pointer filter = FilterType::New();
   CalculatorType::Pointer calculator = CalculatorType::New();  
   filter->SetInput(ctImage_);
-
+  
   uint k = 0;
   double dist = 0.0;
+  //printContact_("B",k,nextContact); // tmp
   do {
     ctImage_->TransformPhysicalPointToIndex(nextContact,nextVoxel);
     filter->SetRegionOfInterest(retriveRegionAroundContact_(nextVoxel,3));
@@ -207,9 +160,48 @@ PhysicalPointType ContactConstructor::getBestContact_(PhysicalPointType prevCont
       return prevContact;
     }
     k++;
+    //printContact_("b",k,nextContact); // tmp
     dist = prevContact.EuclideanDistanceTo(nextContact);
-  } while ((k < 10) && (dist <= 3.8) && (dist >= 2.3));
+  } while ((k < 10) && (dist <= 3.5) && (dist >= 3.0));
   return nextContact;
 }
+
+// TODO 1000 e' una stima a caso, dovrebbe forse essere
+// calcolata sui punti neri della CT? Si prende cioe' una
+// immagine grossa come tutta la CT e poi si itera e si calcola la
+// media/varianza/min/max e si trova la costante
+/* // [TODO] refactoring */
+/* // It tries to find an entry point that does not have null value : */
+/* // 1) First in an incremental region around the entry point if looks */
+/* //    for a voxel that has value != 0. */
+/* // 2) From this point it looks for the point having maximal momentum (p) */
+/* // 3) Then the new entry is the projection of the entry point on the */
+/* //    line from p to target. (i.e. the intersection betwen line p-target */
+/* //    and the line passing for the entry and ortogonal to the line */
+/* //    p-target */
+PhysicalPointType ContactConstructor::computeNewEntry_(PhysicalPointType entry, PhysicalPointType target){
+  int                distance = 5;
+  VoxelPointType     voxelContact;
+  PhysicalPointType  contact = entry;
+  RegionType         region;
+
+  do{
+    ctImage_->TransformPhysicalPointToIndex(contact,voxelContact);
+    region = retriveRegionAroundContact_(voxelContact,distance);
+    itk::ImageRegionIterator<ImageType> imageIterator(ctImage_,region);
+    VoxelPointType p;
+    while(!imageIterator.IsAtEnd()){
+      p = imageIterator.GetIndex();
+      ctImage_->TransformIndexToPhysicalPoint(p,contact);
+      if (ctImage_->GetPixel(p) > 1000) {
+	return getBestContact_(entry, contact);
+      }
+      ++imageIterator;
+    }
+    contact = getContact_(contact,entry,target,1.0);
+  } while(contact.EuclideanDistanceTo(entry) < target.EuclideanDistanceTo(entry));
+  return entry;
+}
+
 
 #endif //CONTACT_CONSTRUCTOR_H
