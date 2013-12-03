d6ba5e6ebd8628be419b568c7694d560e310e993
Gabriele Arnulfo <gabriele.arnulfo@gmail.com>
2013-12-03 15:02:25 +0100

    Provisional classes for AnatomicalPatch and GMPI

---
+++ include/AnatomicalPatchBuilder.h
@@ -0,0 +1,7 @@
+#ifndef ANATOMICAL_PATCH_BUILDER_H
+#define ANATOMICAL_PATCH_BUILDER_H
+
+template class AnatomicalPatchBuilder< class T >{
+
+};
+#endif //ANATOMICAL_PATCH_BUILDER_H
---
+++ include/GMPIEstimator.h
@@ -0,0 +1,7 @@
+#ifndef GMPI_ESTIMATOR_H
+#define GMPI_ESTIMATOR_H
+
+class GMPIEstimator{
+
+};
+#endif //GMPI_ESTIMATOR_H
