d55a68aa2c8a9df7bb15fae2fd980e237c54b7f4
Gabriele Arnulfo <gabriele.arnulfo@gmail.com>
2013-12-05 13:20:58 +0100

    FCSVReader should handle missing information

--- include/FCSVReader.h
+++ include/FCSVReader.h
@@ -45,17 +45,25 @@ private:
   /** read a line from the file that should be a point. */
   PhysicalPointType readPoint(ifstream* file );
 
+	struct fiducialPoint{
+			string name;
+			PhysicalPointType point;
+
+			bool operator==(fiducialPoint a){
+					return name == a.name;
+			}
+	};
+
 };
 
 int FCSVReader::update(void){
   // read from a file as written in filein_
   string str;
-  string name;
-  string prev_name;
+	string name;
   ifstream file;
-  PhysicalPointType target;
-  PhysicalPointType entry;
-  unsigned int k;
+	PhysicalPointType target;
+	PhysicalPointType entry;
+  vector< fiducialPoint > elements;
   
   file.open(filein_->c_str(), ios::in);
 
@@ -67,39 +75,45 @@ int FCSVReader::update(void){
       getline(file,str);
     }
     while(!file.eof()){
-      k = 0;
-      // read two lines at the time.
-      while (k < 2) {
-		// First read the name of the electrode.
-		getline(file,name,',');      
-		// The first 3 are number of interest representing a Point
-		if (k == 0) {
-			target = readPoint(&file);
-			prev_name = name;
-		}
-		else entry = readPoint(&file);
-		// skip what it is left.
-		getline(file,str,'\n');
-		// check whehter prev_name is equal to curr_name
-		// which means that we have an fcsv with t-e pairs 
-		// for each electrode. In case this is not true, returns 0
-		if( prev_name != name )	return 0;
-		k++;
-      }
+				// First read the name of the electrode.
+				getline(file,name,',');      
+				// The first 3 are number of interest representing a Point
 
-      // add the contact only if name exist otherwise it means a blank line was found at the end of fcsv      
-      if( name.length() >= 1) {
-		double distance = (pow(target[0],2.0) + pow(target[1],2.0) + pow(target[2],2.0)) - 
-			(pow(entry[0],2.0) + pow(entry[1],2.0) + pow(entry[2],2.0));    
+				fiducialPoint p;
+				p.name = name;
+				p.point = readPoint(&file);
 
-		// Convert the fcsv points read into the CT space
-		headframe_->fromCenterToRef_(&entry);  // traslation from center to ref space
-		headframe_->fromLPS2RAS_(&entry);      // from the LPS space to a RAS space      
-		headframe_->fromCenterToRef_(&target); // traslation from center to ref space
-		headframe_->fromLPS2RAS_(&target);     // from the LPS space to a RAS space
-		// Create a new electrode with target and entry (swapped if the target point is closer to the Origin)
-		headframe_->addElectrode(Electrode(name,(distance > 0 ? entry : target),(distance > 0 ? target : entry)));
-      }
+				elements.push_back(p);
+
+				// skip what it is left.
+				getline(file,str,'\n');
+		} 
+
+		// check size of both points and names 
+		vector< fiducialPoint >::iterator it;
+
+		for(it=elements.begin(); it != elements.end(); it++){
+			// search for correspondence
+				vector< fiducialPoint>::iterator next_it = find(it+1,elements.end(),*it);
+
+				if( next_it != elements.end()){
+						target = it->point;
+						entry  = next_it->point;
+						name = it->name;
+
+					// add the contact only if name exist otherwise it means a blank line was found at the end of fcsv      
+						double distance = (pow(target[0],2.0) + pow(target[1],2.0) + pow(target[2],2.0)) - 
+							(pow(entry[0],2.0) + pow(entry[1],2.0) + pow(entry[2],2.0));    
+
+						// Convert the fcsv points read into the CT space
+						headframe_->fromCenterToRef_(&entry);  // traslation from center to ref space
+						headframe_->fromLPS2RAS_(&entry);      // from the LPS space to a RAS space      
+						headframe_->fromCenterToRef_(&target); // traslation from center to ref space
+						headframe_->fromLPS2RAS_(&target);     // from the LPS space to a RAS space
+						cout<<name<<" "<<target<<" "<<entry<<" "<<endl;
+						// Create a new electrode with target and entry (swapped if the target point is closer to the Origin)
+						headframe_->addElectrode(Electrode(name,(distance > 0 ? entry : target),(distance > 0 ? target : entry)));
+			}
     }
   }
   return 1;
